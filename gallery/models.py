7f625803df452eff4581e894021ed04367390784
Robert Weber <robertweber95@gmail.com>
2019-07-13 16:55:42 -0700

    gallery: add a model representing a Photo with a title, price, and URL

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Photo represents a photo available for viewing and purchase in the Gallery
+# * title: The name to display for a Photo
+# * price: The cost of this photo for ordering
+# * url  : The static URL where this photo is hosted
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    url   = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.title
--- hlphoto/urls.py
+++ hlphoto/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gallery/', include('gallery.urls')),
+    path('', include('gallery.urls')),
 ]
