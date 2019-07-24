16855e8e5e814195f807d680184c804f0ec78d63
Robert Weber <robertweber95@gmail.com>
2019-07-23 20:53:38 -0700

    Add initial photo list view; add first test for the Photo model

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from gallery.models import Photo
 
-# Initial index view for the gallery
-def index(request):
-    return HttpResponse("Welcome to the Gallery! It is currently under \
-    construction. Please come back soon!")
+class PhotosList(generic.ListView):
+    model = Photo
