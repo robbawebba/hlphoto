c70cade6dec8827237b38da5a67f115b0703dd39
Robert Weber <robertweber95@gmail.com>
2019-07-26 11:51:01 -0700

    remove static files app from project settings

--- hlphoto/settings.py
+++ hlphoto/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'gallery.apps.GalleryConfig',
     'phonenumber_field',
 ]
@@ -119,9 +118,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
